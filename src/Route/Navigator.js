f4a7516bc359eadefe4d84dcf12a27a4fb93c3a1
Najibullah Rizqy F 63 <najibrizqy@gmail.com>
2019-09-20 09:31:37 +0700

    Resolve problem

--- src/Route/Navigator.js
+++ src/Route/Navigator.js
@@ -30,6 +30,7 @@ import LoginPartner from '../Screens/Auth/LoginPartner'
 import RegisterPartner from '../Screens/Auth/RegisterPartner'
 import Editroom from '../Screens/App/Editroom'
 import Editprofile from '../Screens/App/Editprofile'
+
 const AuthStack = createStackNavigator({
   Welcome: WelcomeScreen,
   Login: LoginScreen,
@@ -65,7 +66,6 @@ const AppStack = createStackNavigator({
   Payment : {
     screen: PaymentScreen,
   },
-  KosDetail : KosDetailScreen,
 },{
     header:null,
     headerMode: 'none',
@@ -74,7 +74,6 @@ const AppStack = createStackNavigator({
       if (navigation.state.routes.length > 1) {
         navigation.state.routes.map(route => {
           if (route.routeName === "ProductMap" || route.routeName === "KosDetail" || route.routeName === "Payment") {
-          if (route.routeName === "ProductMap" || route.routeName === "KosDetail") {
             tabBarVisible = false;
           } else {
             tabBarVisible = true;
